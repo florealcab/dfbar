14dc91cfb55dd0c9a1619452fb60da0ba07ae102
joknarf <joknarf@free.fr>
2026-03-07 09:20:15 +0100

    fish....

--- conf.d/dfbar.fish
+++ conf.d/dfbar.fish
@@ -1,4 +1,4 @@
-set __dfb_dir (path resolve (dirname (status --current-filename))/..)
+set __dfb_dir (begin;pushd (dirname (status --current-filename))/..;pwd;popd;end)
 alias dfb="$__dfb_dir/dfb"
 alias df='dfb'
 set -e __dfb_dir
